fe4c8b46b4e2ea1733723650370b88bc2e24a230
Eduardo <el_alienx@hotmail.com>
2023-11-15 00:40:47 +0100

    added friendly date format

--- src/components/ItemLaunch.jsx
+++ src/components/ItemLaunch.jsx
@@ -2,12 +2,17 @@ export default function ItemLaunch({ item }) {
   const { name, date_local, success } = item;
 
   // Properties
+  const friendlyDate = new Date(date_local).toLocaleDateString("en-gb", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   const launchSuccess = success ? "Yay ✅" : "Nay ❌";
 
   return (
     <tr>
       <td>{name}</td>
-      <td>{date_local}</td>
+      <td>{friendlyDate}</td>
       <td>{launchSuccess}</td>
     </tr>
   );
